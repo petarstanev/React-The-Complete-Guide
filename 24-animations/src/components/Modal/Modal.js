2e8e8d29de5aadeff6720521f1b73e9bb83ea524
Petar Stanev <petarstanev@outlook.com>
2023-01-03 17:39:28 +0700

    Add animations app

---
+++ 24-animations/src/components/Modal/Modal.js
@@ -0,0 +1,24 @@
+import React from "react";
+import CSSTransition from "react-transition-group/CSSTransition";
+
+import "./Modal.css";
+
+const animationTiming = {
+  enter: 400,
+  exit: 1000,
+};
+
+const modal = (props) => {
+  return (
+    <CSSTransition in={props.show} timeout={300}>
+      <div className="Modal">
+        <h1>A Modal</h1>
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
+  );
+};
+
+export default modal;
